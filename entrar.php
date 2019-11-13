c253edaf7d917e5da2543094bbfd9ca4d65a1590
pcmacedo <55243555+pcmacedo@users.noreply.github.com>
2019-11-13 16:19:43 -0300

    Update entrar.php

--- entrar.php
+++ entrar.php
@@ -2,7 +2,7 @@
 include ("conecta.php");
 $user=$_REQUEST['usuario'];//usuario veio do html
 $pass=$_REQUEST['senha'];//senha veio do post do html
-$consulta="Select *from usuarios where usuario='$user' and senha='$pass'";
+$consulta="Select *from usuarios where nome='$user' and senha='$pass'";
 $resultado=mysqli_query($link,$consulta)or die("Erro no select");
 if (mysqli_num_rows($resultado)>0)
 	echo "achei";
