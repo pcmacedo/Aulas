1568187b2d2616b5a504958a58d833d5a82dbab7
pcmacedo <55243555+pcmacedo@users.noreply.github.com>
2019-11-13 16:16:54 -0300

    Create entrar.php

---
+++ entrar.php
@@ -0,0 +1,11 @@
+<?php
+include ("conecta.php");
+$user=$_REQUEST['usuario'];//usuario veio do html
+$pass=$_REQUEST['senha'];//senha veio do post do html
+$consulta="Select *from usuarios where usuario='$user' and senha='$pass'";
+$resultado=mysqli_query($link,$consulta)or die("Erro no select");
+if (mysqli_num_rows($resultado)>0)
+	echo "achei";
+else
+	echo "Usuario ou senha invalida";
+?>
